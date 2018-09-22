da6987c8820161ba64aa00fb0c8fde5281688f35
Quan Brew <t@ioover.net>
2018-09-22 23:44:25 +0800

    skills data

---
+++ src/skills.ts
@@ -0,0 +1,12 @@
+import skillData from '../data/skills.yaml';
+
+
+export interface Skill {
+  label: string;
+  name: string;
+  initial: number;
+  contains?: Array<Skill>;
+  tag?: Array<string>;
+}
+
+export const skills = skillData as Skill;
