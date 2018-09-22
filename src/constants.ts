1d23e5104bedfcb4e5398e1a7ecaad85c1f69a86
Quan Brew <t@ioover.net>
2018-09-22 18:53:33 +0800

    action types

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,82 @@
+// *Quick Reference: Investigator Generation* Rulebook P34
+
+
+// System
+export const SAVE = 'SAVE';
+export const LOAD = 'LOAD';
+export const RESET = 'RESET';
+export const CHANGE_ERA = 'CHANGE_ARE';
+export const CUSTOM_ERA = 'CUSTOM_ERA';
+export const ROLL = 'ROLL';
+
+// Attribute
+export const AUTO_ATTRIBUTE = 'AUTO_ATTRIBUTE';
+export const ENABLE_POINT_BUY = 'ENABLE_BUY_POINT'; // P48
+export const DISABLE_POINT_BUY = 'DISABLE_POINT_BUY';
+export const EDIT_ATTRIBUTE = 'EDIT_ATTRIBUTE';
+export const APPLY_MODIFIER = 'APPLY_MODIFIER';
+export const AUTO_AGE = 'AUTO_AGE';
+export const EDIT_AGE = 'EDIT_AGE';
+export const EDIT_HP = 'EDIT_HP';
+export const EDIT_MP = 'EDIT_MP';
+export const EDIT_SAN = 'EDIT_SAN';
+export const RECOVER_LUCK = 'RECOVER_LUCK'; // P99
+
+// Skill
+export const SET_SKILL_LIMIT = 'SET_SKILL_LIMIT';
+export const ADD_SKILL = 'ADD_SKILL';
+export const EDIT_SKILL = 'EDIT_SKILL';
+export const REMOVE_SKILL = 'REMOVE_SKILL';
+export const SKILL_FILTER = 'SKILL_FILTER';
+export const CHANGE_CTHULHU_MYTHOS = 'CHANGE_CTHULHU_MYTHOS';
+export const CHANGE_CREDIT_RATING = 'CHANGE_CREDIT_RATING';
+export const INVESTIGATOR_DEVELOPMENT_PHASE = 'INVESTIGATOR_DEVELOPMENT_PHASE';
+
+// Avatar
+export const UPLOAD_AVATAR = 'UPLOAD_AVATAR';
+export const CLEAR_AVATAR = 'CLEAR_AVATAR';
+
+// Backstory
+export const GENERATE_BACKSTORY_HINT = 'GENERATE_BACKSTORY_HINT';
+export const EDIT_BACKSTORY = 'EDIT_BACKSTORY';
+export const SELECT_KEY_CONNECTION = 'SELECT_KEY_CONNECTION'; // P169
+
+// Note
 export const EDIT_NOTE = 'EDIT_NOTE';
+export const ARCHIVE_NOTE = 'ARCHIVE_NOTE';
+export const SELECT_NOTE = 'SELECT_NOTE';
+export const CREATE_NOTE = 'CREATE_NOTE';
+export const RENAME_NOTE = 'RENAME_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
+
+
+// Equipment
+export const ADD_EQUIPMENT = 'ADD_EQUIPMENT';
+export const REMOVE_EQUIPMENT = 'REMOVE_EQUIPMENT';
+
+// Remark
+export const ADD_REMARK = 'ADD_REMARK';
+export const REMOVE_REMARK = 'REMOVE_REMARK';
+
+
+// Cash & Assets
+export const CHANGE_SPENDING_LEVEL = 'CHANGE_SPENDING_LEVEL'; // P96
+export const SET_CASH = 'SET_CASH';
+export const ADD_ASSET = 'ADD_ASSET';
+export const REMOVE_ASSET = 'REMOVE_ASSET';
+
+
+// Occupation
+export const DECIDE_OCCUPATION = 'DECIDE_OCCUPATION';
+export const CREATE_OCCUPATION = 'CREATE_OCCUPATION';
+
+// Fellow
+export const ADD_FELLOW = 'ADD_FELLOW';
+export const EDIT_FELLOW = 'EDIT_FELLOW';
+export const REMOVE_FELLOW = 'REMOVE_FELLOW';
+
+
+// Weapon
+export const ADD_WEAPON = 'ADD_WEAPON';
+export const EDIT_WEAPON = 'EDIT_WEAPON';
+export const REMOVE_WEAPON = 'REMOVE_WEAPON';
