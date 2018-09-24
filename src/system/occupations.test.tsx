a1a9761f743a5975c9322f382627a7259315548a
Quan Brew <t@ioover.net>
2018-09-24 17:18:09 +0800

    read YAML data in test

--- src/system/occupations.test.tsx
+++ src/system/occupations.test.tsx
@@ -1,12 +1,17 @@
 import { computeSkillPoint } from './interpreter';
 import { autoAttributes } from './attributes';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import { Occupation } from "./occupations";
+
+
+const file = fs.readFileSync('./data/occupations.yaml', 'utf8');
+const occupations = yaml.safeLoad(file) as Array<Occupation>;
+
 
 it('compute skill point', () => {
-  const result = [
-    'EDU * 4',
-    'EDU * 2 + DEX * 2',
-    'EDU * 2 + STRorDEX * 2',
-    'EDU * 2 + APPorDEXorSTR * 2',
-  ].map(ptn => computeSkillPoint(autoAttributes(), ptn)).filter(x => x === null || x < 50);
+  const result = occupations
+    .map(occupation => computeSkillPoint(autoAttributes(), occupation.skillPoint))
+    .filter(x => x === null || x < 50);
   expect(result).toEqual([]);
 });
