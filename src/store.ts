4c27ee83aff49ef7bccd4bdf7f10d1f6d274a4e7
Quan Brew <t@ioover.net>
2018-09-24 20:17:42 +0800

    logger middleware

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,20 @@
-import { createStore } from 'redux';
+import {applyMiddleware, createStore, Middleware} from 'redux';
 import { sheet } from './reducers';
+
+
+const logger: Middleware = store => next => action => {
+  console.group(action.type);
+  console.info(action);
+  let result = next(action);
+  // console.log('next state', store.getState());
+  console.groupEnd();
+  return result
+};
+
 
 export const store = createStore(
   sheet,
   // @ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger),
 );
