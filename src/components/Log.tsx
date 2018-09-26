56e78000285ef0d3a3fc39b5c56c9260b2d2cea8
Quan Brew <t@ioover.net>
2018-09-26 14:19:40 +0800

    log style

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { Sheet } from "../system/sheet";
 import { Modified } from "../system/logger";
 import { LogRecord } from "../system/logger";
-import { Icon, Typography } from "@material-ui/core";
+import { Icon, List, ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
 
 
 interface Props {
@@ -21,21 +21,36 @@ class Log extends React.Component<Props, State> {
   static modified(record: Modified) {
     const remark = record.remark ? <span>({record.remark})</span> : null;
     if (record.old === undefined) {
-      return <Typography><Icon fontSize='inherit'>add</Icon> {record.display} {record.next} {remark}</Typography>
+      return (
+        <>
+          <ListItemIcon><Icon fontSize='inherit'>add</Icon></ListItemIcon>
+          <ListItemText>{record.display} {record.next} {remark}</ListItemText>
+        </>
+      );
     }
-    return <Typography><Icon fontSize='inherit'>edit</Icon> {record.display} {record.old} <Icon fontSize='inherit'>arrow_forward</Icon> {record.next} {remark}</Typography>;
+    else {
+      const arrow = <Icon fontSize='inherit'>arrow_forward</Icon>;
+      return (
+        <>
+          <ListItemIcon><Icon fontSize='inherit'>edit</Icon></ListItemIcon>
+          <ListItemText>{record.display} {record.old} {arrow} {record.next} {remark}</ListItemText>
+        </>
+      );
+    }
   }
 
   render() {
     const logs = this.props.logs.map((record: LogRecord, index: number) => {
       switch (record.type) {
-        case 'Modified': return (<li key={index}>{Log.modified(record)}</li>);
+        case 'Modified': return (<ListItem key={index}>{Log.modified(record)}</ListItem>);
       }
     });
     return (
-      <ul>
-        { logs }
-      </ul>
+      <div>
+        <List>
+          { logs }
+        </List>
+      </div>
     );
   }
 }
--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -3,16 +3,21 @@ import Note from './Note';
 import InformationForm from './InformationForm';
 import Attributes from "./AttributesForm";
 import Log from "./Log";
+import { Grid } from "@material-ui/core";
 
 
 const Sheet = () => {
   return (
-    <div>
-      <InformationForm />
-      <Attributes />
-      <Note />
-      <Log />
-    </div>
+    <Grid container >
+      <Grid item md={8} lg={10}>
+        <InformationForm />
+        <Attributes />
+        <Note />
+      </Grid>
+      <Grid item md={4} lg={2}>
+        <Log />
+      </Grid>
+    </Grid>
   );
 };
 
